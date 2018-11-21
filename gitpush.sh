fbdf831f998e49831c18aa640da09619c0560224
theIDinside <simon.farre.cx@gmail.com>
2018-11-21 22:58:41 +0100

    "Testing gitpush script"

---
+++ gitpush.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Add commit message for submodule: "
+read sc_msg
+cd ./deps/command_prompt/
+git add -A
+git commit -m "$sc_msg"
+cd ..
+cd ..
+echo "Add commit message for parent repo: "
+read parent_repo_msg
+git add -A
+git add ./deps/command_prompt/
+git commit -m "$parent_repo_msg"
+git push origin master --recurse-submodules=on-demand
+
+
